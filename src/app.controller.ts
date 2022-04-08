e221125617110a40704d6432f32a85050c9fadd9
villarroel-cf <villarroel.cf@gmail.com>
2022-04-08 17:13:03 -0400

    Se ha separado los metodos de los controladores products y categories

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -17,31 +17,5 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'con/sas/';
-  }
-
-  @Get('/products/:id')
-  /*getProduct(@Param() params: any) {
-    return `Product ${params.id}`;
-  }*/
-  getProduct(@Param('id') id: string) {
-    return `Product ${id}`;
-  }
-
-  @Get('/categories/:id/products/:productId')
-  getCategory(@Param('id') id: string, @Param('productId') productId: string) {
-    return `category ${id} and product ${productId}`;
-  }
-
-  @Get('products')
-  /*getProducts(@Query() params: any) {
-    const { limit, offset } = params;
-    return `Products limit=> ${limit} offset => ${offset}`;
-  }*/
-  getProducts(
-    @Query('limit') limit = 100,
-    @Query('offset') offset = 0,
-    @Query('brand') brand: string,
-  ) {
-    return `Products limit=> ${limit} offset => ${offset} marca => ${brand}`;
   }
 }
--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Param } from '@nestjs/common';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get(':id')
+  /*getProduct(@Param() params: any) {
+    return `Product ${params.id}`;
+  }*/
+  getProduct(@Param('id') id: string) {
+    return `Product ${id}`;
+  }
+  @Get()
+  /*getProducts(@Query() params: any) {
+    const { limit, offset } = params;
+    return `Products limit=> ${limit} offset => ${offset}`;
+  }*/
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `Products limit=> ${limit} offset => ${offset} marca => ${brand}`;
+  }
+}
